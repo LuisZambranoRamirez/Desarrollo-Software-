6278a90220de8abaf09a2d10f352d1a57820b907
l <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-06-29 10:10:03 -0500

    fix: corregir importaciones

--- Backend/app/config/config.py
+++ Backend/app/config/config.py
@@ -8,7 +8,7 @@ class Settings:
     DB_PORT = os.getenv("DB_PORT", "5432")
     DB_NAME = os.getenv("DB_NAME", "clinica")
     DB_USER = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "drakotako1234")
 
     DATABASE_URL = (
         f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
